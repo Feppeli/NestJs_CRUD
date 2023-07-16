8d30109315e144aa2b3af6181603d942598c7ab4
Fernanda <mfsoreis18@gmail.com>
2023-07-15 23:21:07 -0300

    sequelize

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,22 @@ import { Module } from '@nestjs/common';
 import { adminController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsModule } from './cats/cats.module';
+import { SequelizeModule } from '@nestjs/sequelize';
 
 @Module({
   controllers: [adminController],
   providers: [AppService],
-  imports: [CatsModule],
+  imports: [
+    CatsModule,
+    SequelizeModule.forRoot({
+      dialect: 'postgres',
+      host:'localhost',
+      port:5432,
+      username:'postgres',
+      password:'root',
+      database:'test',
+      models:[]
+    })
+  ],
 })
 export class AppModule {}
